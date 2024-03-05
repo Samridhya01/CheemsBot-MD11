81e3cdd521c92de8ec382bf7fb4a18252a5c6fec
Debashis121212 <139324072+Debashis121212@users.noreply.github.com>
2024-03-05 12:50:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.botscript = 'https://github.com/Debashis121212/CheemsBot-MD11' //script l
 global.packname = "Sticker By"
 global.author = "Debashis Dey\n\n+919339619072"
 global.creator = "919339619072 @s.whatsapp.net"
-global.xprefix = '','!','.','#','&'
+global.xprefix = '.'
 global.premium = ["919339619072"] // Premium User
 global.hituet = 0
 
